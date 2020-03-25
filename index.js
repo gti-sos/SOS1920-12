a32e7701761bbd67a029488abd2a07437592bad0
wuaho <juanjoserequenalama@gmail.com>
2020-03-25 13:39:12 +0000

    api overdose_deaths added

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-
-
 const cool = require("cool-ascii-faces");
 const express = require("express");
+const bodyParser = require("body-parser");
+const BASE_API_URL = "/api/v1";
 
 var app = express();
+var port = process.env.PORT || 80;
 
-var port = process.env.PORT || 8080;
-
+//La siguiente linea hace que cada vez que detecte un json, te lo transforme en una variable
+app.use(bodyParser.json());
 app.use("/",express.static("./public"));
 
-app.get("/cool",(request, response) => {
+
+app.get("/cool",(request,response) => {
 	response.send("<html>"+cool()+"</html>");
 });
 
@@ -25,8 +27,153 @@ app.get("/time",(request, response) => {
 	response.send("<html>"+dateTime+"</html>");
 });
 
-app.listen(port, () =>{
-	console.log("Servidor preparado y listo");	
+// -------------------------------------OVERDOSE_DEATHS API-------------------------------------
+
+var overdose_deaths = [];
+
+var initialOverdose_deaths = [
+	{
+		country: "Austria",
+		year: 2017,
+		death_male: 125,
+		death_female: 29,
+		death_total: 154,
+		mean_age: 36.7
+	},
+	{
+		country: "Croatia",
+		year: 2016,
+		death_male: 44,
+		death_female: 12,
+		death_total: 56,
+		mean_age: 37.1
+	},
+	{
+		country: "Germany",
+		year: 2015,
+		death_male: 1001,
+		death_female: 191,
+		death_total: 1226,
+		mean_age:38.1
+	},
+	{
+		country: "Italy",
+		year: 2017,
+		death_male: 253,
+		death_female: 41,
+		death_total: 294,
+		mean_age:39.4
+	}
+];
+
+
+// load initial overdose_deaths
+
+app.get(BASE_API_URL+"/overdose_deaths/loadInitialData", (request,response) =>{
+	if(overdose_deaths.length>=1){
+		response.sendStatus(409, "CONFLICT(this action would remove the existing data)");
+	}
+	else{
+		overdose_deaths = initialOverdose_deaths;
+		response.send(JSON.stringify(overdose_deaths, null, 2));
+	}
 });
 
-console.log("Iniciando servidor...");
+//POST overdose_deaths
+app.post(BASE_API_URL+"/overdose_deaths",(request, response) =>{
+	
+	var newOverdoseDeaths = request.body;
+	
+	if( (newOverdoseDeaths==null) || (newOverdoseDeaths.country == null) || (newOverdoseDeaths.year == null) ||(newOverdoseDeaths.death_total == null)  ){
+		response.sendStatus(400, "BAD REQUEST(data provided not correctly)");
+	}
+	else{
+		overdose_deaths.push(newOverdoseDeaths);
+		response.sendStatus(201,"CREATED");
+	}
+	
+});
+
+//GET overdose_deaths
+app.get(BASE_API_URL+"/overdose_deaths",(request, response)=> {
+	
+	if(overdose_deaths.length <1){
+		response.sendStatus(400,"BAD REQUEST(data is empty)");
+	}else{
+		response.send(JSON.stringify(overdose_deaths,null,2));
+	}
+});
+
+//PUT overdose_deaths
+app.put(BASE_API_URL+"/overdose_deaths",(request, response) =>{
+	response.sendStatus(405,"METHOD NOT ALLOWED")
+});
+
+//DELETE overdose_deaths
+app.delete(BASE_API_URL+"/overdose_deaths",(request, response)=> {
+	overdose_deaths = [];
+	response.sendStatus(200, "OK");
+});
+
+//POST overdose_deaths/XXX
+app.post(BASE_API_URL+"/overdose_deaths/:country",(request, response)=> {
+	response.sendStatus(405, "METHOD NOT ALLOWED")
+});
+
+//GET overdose_deaths/XXX
+app.get(BASE_API_URL+"/overdose_deaths/:country",(request, response)=> {
+	
+	var country = request.params.country;
+	var filteredOverdose_deaths = overdose_deaths.filter( (c) => {
+		return (c.country == country);
+	});
+	
+	if(filteredOverdose_deaths.length >= 1){
+		response.send(filteredOverdose_deaths[0]);
+	}else{
+		response.sendStatus(404,"NOT FOUND");
+	}
+});
+
+//PUT overdose_deaths/XXX
+app.put(BASE_API_URL+"/overdose_deaths/:country/:year",(request, response)=> {
+	var country = request.params.country;
+	var year = request.params.year;
+	
+	var newOverdoseDeaths = request.body;
+	
+	if(country!=newOverdoseDeaths.country || year != newOverdoseDeaths.year){
+		response.sendStatus(400, "BAD REQUEST(data does not match)");
+	}else{
+		var filteredOverdose_deaths = overdose_deaths.filter( (c) => {
+		return (!((c.country == country) && (c.year == year)));
+		});
+		overdose_deaths = filteredOverdose_deaths;
+		overdose_deaths.push(newOverdoseDeaths);
+		response.sendStatus(200, "OK");
+	}
+});
+
+//DELETE overdose_deaths/XXX
+app.delete(BASE_API_URL+"/overdose_deaths/:country/:year",(request, response)=> {
+	var country = request.params.country;
+	var year = request.params.year;
+	
+	var filteredOverdose_deaths = overdose_deaths.filter( (c) => {
+		return (!((c.country == country) && (c.year == year)));
+	});
+	
+	if(filteredOverdose_deaths.length < overdose_deaths.length){
+		overdose_deaths = filteredOverdose_deaths;
+		response.sendStatus(200, "OK");
+	}else{
+		response.sendStatus(404,"NOT FOUND");
+	}
+	
+});
+
+app.listen(port, () => {
+	console.log("Server ready");
+});
+
+console.log("Starting server...");
