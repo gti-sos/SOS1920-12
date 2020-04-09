66866b7533efcd30d236071c5eca197bb6f70a28
Juanjo <juanjoserequenalama@gmail.com>
2020-04-09 20:44:10 +0200

    overdose-deaths get now has searchs, limit and offset

--- overdoseDeathsAPI/index.js
+++ overdoseDeathsAPI/index.js
@@ -4,7 +4,6 @@ module.exports = function (app) {
 	const dataStore = require("nedb");
 	const path = require("path");
 	const BASE_API_URL = "/api/v1";
-	
 	const dbFileName = path.join(__dirname,"overdoseDeaths.db");
 	
 	const db = new dataStore({
@@ -12,7 +11,7 @@ module.exports = function (app) {
 		autoload : true
 });
 	
-var overdose_deaths = [];
+// antes: var overdose_deaths = [];
 
 var initialOverdose_deaths = [
 	{
@@ -50,30 +49,85 @@ var initialOverdose_deaths = [
 ];
 
 
-// load initial overdose-deaths
+// GET load initial overdose-deaths
 
-app.get(BASE_API_URL + "/overdose-deaths/loadInitialData", (request, response) => {
+app.get(BASE_API_URL + "/overdose-deaths/loadInitialData", (req, res) => {
 	
 	/*
-	CAMBIAR CODIGO PARA QUE HAGA LO MISMO PERO CON LA BASE DE DATOS
+	ANTES SIN LA BASE DE DATOS TENIA EL AVISO 409 PERO NO SE COMO PONERLO JUNTO, TAMPOCO ES MUY NECESARIO 
+	NO PERMITIR QUE SE PUEDA CARGAR INFINITAMENTE LOS DATOS 
+
 	if (overdose_deaths.length >= 1) {
-		response.sendStatus(409, "CONFLICT(this action would remove the existing data)");
+		res.sendStatus(409, "CONFLICT(this action would remove the existing data)");
 	}
 	else {
 		overdose_deaths = initialOverdose_deaths;
-		response.send(JSON.stringify(overdose_deaths, null, 2));
+		res.send(JSON.stringify(overdose_deaths, null, 2));
 		}
 		*/
+
 	console.log("New GET .../loadInitialData");
 	db.insert(initialOverdose_deaths);
-	response.sendStatus(200);
-	console.log("Initial Overdose-deaths loaded"+JSON.stringify(initialOverdose_deaths, null, 2));
+	res.sendStatus(200);
+	console.log("Initial overdose-deaths loaded"+JSON.stringify(initialOverdose_deaths, null, 2));
+});
+
+//GET overdose-deaths
+app.get(BASE_API_URL + "/overdose-deaths", (req, res) => {
 	
+	//This property is an object containing a property for each query string parameter in the route. If there is no query string, it is the empty object, {}.
+	// For example  for /api/v1/overdose-deaths?country=Germany&year=2015 the result would be query = { country: 'Germany', year: '2015' }
+	//Podemos acceder a cada valor con query.clave  e.g. query.country nos devolveria 'Germany'
+
+	var query = req.query;
+
+	//Obtenemos los offset y limit desde la query, si estan vacios devuelven null (NaN, aka Not a Number) y los quitamos de la query para que no nos molesten (estas no tenemos que parsearlas)
+	var offset = query.offset;
+	var limit = query.limit
+
+	delete query.offset;
+	delete query.limit;
+
+	//Si la query contiene alguno de los atributos numericos, tenemos que pasar estos atributos de string a integer o double
+	//Comprobamos primero que la query tiene alguno de esos atributos
 	
+	if(query.hasOwnProperty("year")){
+		query.year = parseInt(query.year);	
+	}
+	if(query.hasOwnProperty("death_male")){
+		query.death_male = parseInt(query.death_male);
+	}
+	if(query.hasOwnProperty("death_female")){
+		query.death_female = parseInt(query.death_female);
+	}
+	if(query.hasOwnProperty("death_total")){
+		query.death_total = parseInt(query.death_total);
+	}
+	if(query.hasOwnProperty("mean_age")){
+		query.mean_age = parseFloat(query.mean_age);
+	}
+
+	console.log("New GET .../overdose-deaths");
+
+	//err y overdose_deaths son: el error que devuelve y el array con las muertes que satisfacen la query 
+	// por otra parte, .skip nos consigue el offset deseado y .limit pues lo mismo pero con el limit
+
+	db.find(query).skip(offset).limit(limit).exec((err,overdose_deaths) => {
+		//Si no queremos enseñar el id de cada objeto en la base de datos:
+		overdose_deaths.forEach( (c) =>{
+			delete c._id;
+	});	
+
+	if (overdose_deaths.length < 1) {
+		res.sendStatus(400, "BAD REQUEST(data is empty)");}
+	else{
+		res.send(JSON.stringify(overdose_deaths, null, 2));
+		console.log("Data sent:"+JSON.stringify(overdose_deaths, null, 2));
+	}});
 });
 
 //POST overdose-deaths
-app.post(BASE_API_URL + "/overdose-deaths", (request, response) => {
+app.post(BASE_API_URL + "/overdose-deaths", (req, res) => {
  
 	var newOverdoseDeaths = request.body;
 
@@ -88,31 +142,8 @@ app.post(BASE_API_URL + "/overdose-deaths", (request, response) => {
 
 });
 
-//GET overdose-deaths
-app.get(BASE_API_URL + "/overdose-deaths", (request, response) => {
-	
-	/* IGUAL, ADAPTAR CODIGO
-	if (overdose_deaths.length < 1) {
-		response.sendStatus(400, "BAD REQUEST(data is empty)");
-	} else {
-		response.send(JSON.stringify(overdose_deaths, null, 2));
-	}
-	*/
-	
-	console.log("New GET .../overdose-deaths");
-	//err y deaths son el error que devuelve y el array con las muertes que satisfacen la query 
-	db.find({}, (err,deaths) =>{
-		
-		//Si no queremos enseñar el id de cada objeto en la base de datos:
-		deaths.forEach( (c) =>{
-			delete c._id;
-			});	
-		
-	response.send(JSON.stringify(deaths, null, 2));
-	console.log("Data sent:"+JSON.stringify(deaths, null, 2));
-	});
-	
-});
+
+
 
 //PUT overdose-deaths
 app.put(BASE_API_URL + "/overdose-deaths", (request, response) => {
@@ -123,11 +154,6 @@ app.put(BASE_API_URL + "/overdose-deaths", (request, response) => {
 app.delete(BASE_API_URL + "/overdose-deaths", (request, response) => {
 	overdose_deaths = [];
 	response.sendStatus(200, "OK");
-});
-
-//POST overdose-deaths/XXX
-app.post(BASE_API_URL + "/overdose-deaths/:country", (request, response) => {
-	response.sendStatus(405, "METHOD NOT ALLOWED")
 });
 
 //GET overdose-deaths/XXX
@@ -144,6 +170,13 @@ app.get(BASE_API_URL + "/overdose-deaths/:country", (request, response) => {
 		response.sendStatus(404, "NOT FOUND");
 	}
 });
+
+//POST overdose-deaths/XXX
+app.post(BASE_API_URL + "/overdose-deaths/:country", (request, response) => {
+	response.sendStatus(405, "METHOD NOT ALLOWED")
+});
+
+
 
 //PUT overdose-deaths/XXX
 app.put(BASE_API_URL + "/overdose-deaths/:country/:year", (request, response) => {
