a04f673381d21e9a5c96e8d537aaa63ba8177904
Requena115 <reque1993@gmail.com>
2020-04-16 20:37:33 +0000

    including school dropouts API

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const cool = require("cool-ascii-faces");
 const overdoseDeathsAPI = require(path.join(__dirname,"overdoseDeathsAPI"));
 const drugOffencesAPI = require(path.join(__dirname,"drugOffencesAPI"));
+const schoolDropoutsAPI = require(path.join(__dirname,"schoolDropoutsAPI"));
 
 const port = process.env.PORT || 80;
 const app = express();
@@ -18,155 +19,10 @@ const BASE_API_URL = "/api/v1";
 //INICIO DE DE TODAS LAS APIs
 
 overdoseDeathsAPI(app);
-drugOffencesAPI(app)
+drugOffencesAPI(app);
+schoolDropoutsAPI(app);
 
-
-
-// -------- -------------------------- school-dropouts AP start ------------------------ ----------
-var school_dropouts = [];
-var school_dropouts_sample = [
-	{
-		country: "Austria",
-		year: 2002,
-		sd_all: 9.5,
-		sd_mas: 8.7,
-		sd_fem: 10.2
-	},
-	{
-		country: "Spain",
-		year: 2002,
-		sd_all: 30.9,
-		sd_mas: 37.2,
-		sd_fem: 24.3
-	},
-	{
-		country: "Finland",
-		year: 2003,
-		sd_all: 10.1,
-		sd_mas: 12,
-		sd_fem: 8.2
-	},
-	{
-		country: "Italy",
-		year: 2003,
-		sd_all: 23,
-		sd_mas: 26.5,
-		sd_fem: 19.3
-	},
-	{
-		country: "Spain",
-		year: 2016,
-		sd_all: 19,
-		sd_mas: 22.7,
-		sd_fem: 15.1
-	}
-];
-
-
-// load initial school_dropouts_sample
-
-app.get(BASE_API_URL + "/school-dropouts/loadInitialData", (request, response) => {
-	if (school_dropouts.length >= 1) {
-		response.sendStatus(409, "CONFLICT(this action would remove the existing data)");
-	}
-	else {
-		school_dropouts = school_dropouts_sample;
-		response.send(JSON.stringify(school_dropouts, null, 2));
-	}
-});
-
-//POST school_dropouts
-app.post(BASE_API_URL + "/school-dropouts", (request, response) => {
-	var auxSchoolDropouts = request.body;
-
-	if ((auxSchoolDropouts == null) || (auxSchoolDropouts.country == null) || (auxSchoolDropouts.year == null) || (auxSchoolDropouts.sd_all == null)) {
-		response.sendStatus(400, "BAD REQUEST(data provided not correctly)");
-	}
-	else {
-		school_dropouts.push(auxSchoolDropouts);
-		response.sendStatus(201, "CREATED");
-	}
-
-});
-
-//GET school_dropouts
-app.get(BASE_API_URL + "/school-dropouts", (request, response) => {
-
-	if (school_dropouts.length < 1) {
-		response.sendStatus(400, "BAD REQUEST(data is empty)");
-	} else {
-		response.send(JSON.stringify(school_dropouts, null, 2));
-	}
-});
-
-//PUT school_dropouts
-app.put(BASE_API_URL + "/school-dropouts", (request, response) => {
-	response.sendStatus(405, "METHOD NOT ALLOWED")
-});
-
-//DELETE school_dropouts
-app.delete(BASE_API_URL + "/school-dropouts", (request, response) => {
-	school_dropouts = [];
-	response.sendStatus(200, "OK");
-});
-
-//POST school_dropouts/country
-app.post(BASE_API_URL + "/school-dropouts/:country", (request, response) => {
-	response.sendStatus(405, "METHOD NOT ALLOWED")
-});
-
-//GET school_dropouts/country
-app.get(BASE_API_URL + "/school-dropouts/:country", (request, response) => {
-	var country = request.params.country;
-	var school_dropouts_filtered = school_dropouts.filter((c) => {
-		return (c.country == country);
-	});
-
-	if (school_dropouts_filtered.length >= 1) {
-		response.send(school_dropouts_filtered[0]);
-	} else {
-		response.sendStatus(404, "NOT FOUND");
-	}
-});
-
-//PUT school_dropouts/country/year
-app.put(BASE_API_URL + "/school-dropouts/:country/:year", (request, response) => {
-	var country = request.params.country;
-	var year = request.params.year;
-
-	var aux_school_dropout = request.body;
-
-	if (country != aux_school_dropout.country || year != aux_school_dropout.year) {
-		response.sendStatus(400, "BAD REQUEST(data does not match)");
-	} else {
-		var school_dropouts_filtered = school_dropouts.filter((s) => {
-			return (!((s.country == country) && (s.year == year)));
-		});
-		school_dropouts = school_dropouts_filtered;
-		school_dropouts.push(aux_school_dropout);
-		response.sendStatus(200, "OK");
-	}
-});
-
-
-//DELETE school_dropouts/country/year
-app.delete(BASE_API_URL + "/school-dropouts/:country/:year", (request, response) => {
-	var country = request.params.country;
-	var year = request.params.year;
-
-	var school_dropouts_filtered = school_dropouts.filter((c) => {
-		return (!((c.country == country) && (c.year == year)));
-	});
-
-	if (school_dropouts_filtered.length < school_dropouts.length) {
-		school_dropouts = school_dropouts_filtered;
-		response.sendStatus(200, "OK");
-	} else {
-		response.sendStatus(404, "NOT FOUND");
-	}
-
-});
-// -------------------------------------END_OVERDOSE_DEATHS API-------------------------------------
+// -------------------------------------END APIS-------------------------------------
 
 app.get("/cool", (request, response) => {
 	response.send("<html>" + cool() + "</html>");
---
+++ schoolDropoutsAPI/index.js
@@ -0,0 +1,242 @@
+// -------------------------------------SCHOOL-DROPOUTS API-------------------------------------
+module.exports = function (app) {
+	console.log("Registering school-dropouts API...");
+	const dataStore = require("nedb");
+	const path = require("path");
+	const BASE_API_URL = "/api/v1";
+	const dbFileName = path.join(__dirname,"schoolDroupouts.db");
+	
+	const db = new dataStore({
+		filename : dbFileName, 
+		autoload : true
+});
+
+var initialSchool_dropouts = [
+	{
+		country: "Austria",
+		year: 2002,
+		sd_all: 9.5,
+		sd_mas: 8.7,
+		sd_fem: 10.2
+	},
+	{
+		country: "Spain",
+		year: 2002,
+		sd_all: 30.9,
+		sd_mas: 37.2,
+		sd_fem: 24.3
+	},
+	{
+		country: "Finland",
+		year: 2003,
+		sd_all: 10.1,
+		sd_mas: 12,
+		sd_fem: 8.2
+	},
+	{
+		country: "Italy",
+		year: 2003,
+		sd_all: 23,
+		sd_mas: 26.5,
+		sd_fem: 19.3
+	},
+	{
+		country: "Spain",
+		year: 2016,
+		sd_all: 19,
+		sd_mas: 22.7,
+		sd_fem: 15.1
+	}
+];
+
+
+// GET load initial school-dropouts
+
+app.get(BASE_API_URL + "/school-dropouts/loadInitialData", (req, res) => {
+
+	console.log("New GET .../loadInitialData");
+	var school_dropouts = db.getAllData();
+	
+	if (school_dropouts.length >= 1) {
+		res.sendStatus(409, "CONFLICT(this action would remove the existing data)");
+		console.log("There is already loaded data");
+	}else{
+		db.insert(initialSchool_dropouts);
+		res.send(JSON.stringify(initialSchool_dropouts, null, 2));
+		console.log("Initial school-dropouts data loaded"+JSON.stringify(initialSchool_dropouts, null, 2));
+	}
+});
+
+//GET school-dropouts
+app.get(BASE_API_URL + "/school-dropouts", (req, res) => {
+	
+	console.log("New GET .../school-dropouts");
+
+	var query = req.query;
+
+	var offset = query.offset;
+	var limit = query.limit
+
+	delete query.offset;
+	delete query.limit;
+
+	
+	if(query.hasOwnProperty("year")){
+		query.year = parseInt(query.year);	
+	}
+	if(query.hasOwnProperty("sd_all")){
+		query.sd_all = parseInt(query.sd_all);
+	}
+	if(query.hasOwnProperty("sd_mas")){
+		query.sd_mas = parseInt(query.sd_mas);
+	}
+	if(query.hasOwnProperty("sd_fem")){
+		query.sd_fem = parseInt(query.sd_fem);
+	}
+
+
+	db.find(query).skip(offset).limit(limit).exec((err,school_dropouts) => {
+		school_dropouts.forEach( (c) =>{
+			delete c._id;
+	});	
+
+	if (school_dropouts.length < 1) {
+		res.sendStatus(400, "BAD REQUEST(data is empty)");
+		console.log("The requested data is empty");
+	}
+	else{
+		res.send(JSON.stringify(school_dropouts, null, 2));
+		console.log("Data sent:"+JSON.stringify(school_dropouts, null, 2));
+	}});
+	
+});
+
+//POST school-dropouts
+app.post(BASE_API_URL + "/school-dropouts", (req, res) => {
+	console.log("New POST .../school-dropouts");
+
+	var newSchoolDroupouts = req.body;
+
+	if ((newSchoolDroupouts == null) || (newSchoolDroupouts.country == null) || (newSchoolDroupouts.year == null) || (newSchoolDroupouts.sd_all == null)) {
+		res.sendStatus(400, "BAD REQUEST(data not correctly provided )");
+		console.log("The data wasnt correctly provided");
+	}
+	else {
+		db.insert(newSchoolDroupouts);
+		res.sendStatus(201, "CREATED");
+		console.log("Data input:"+JSON.stringify(newSchoolDroupouts, null, 2));
+	}
+});
+
+//PUT school-dropouts
+app.put(BASE_API_URL + "/school-dropouts", (req, res) => {
+	res.sendStatus(405, "METHOD NOT ALLOWED")
+});
+
+//DELETE school-dropouts
+app.delete(BASE_API_URL + "/school-dropouts", (req, res) => {
+	console.log("New DELETE .../school-dropouts");
+
+	db.remove({}, { multi: true }, function (err, numRemoved) {
+	});
+	res.sendStatus(200, "OK");
+	console.log("Data successfully removed");
+});
+
+//GET school-dropouts/:country
+app.get(BASE_API_URL + "/school-dropouts/:country", (req, res) => {
+	console.log("New GET .../school-dropouts/:country");
+
+	var country = req.params.country;
+	var query = {"country":country};
+
+	db.find(query).exec((err,school_dropouts) => {
+		if (school_dropouts.length >= 1) {
+			delete school_dropouts[0]._id;
+			res.send(JSON.stringify(school_dropouts[0], null, 2));
+			console.log("Data sent:"+JSON.stringify(school_dropouts[0], null, 2));
+		} else {
+			res.sendStatus(404, "NOT FOUND");
+			console.log("The data requested is empty");
+		}
+	});
+});
+
+//GET school-dropouts/:country/:year
+app.get(BASE_API_URL + "/school-dropouts/:country/:year", (req, res) => {
+	console.log("New GET .../school-dropouts/:country/:year");
+
+	var country = req.params.country;
+	var year = req.params.year;
+	var query = {"country":country, "year":parseInt(year)};
+
+	db.find(query).exec((err,school_dropouts) => {
+		if (school_dropouts.length >= 1) {
+			delete school_dropouts[0]._id;
+			res.send(JSON.stringify(school_dropouts[0], null, 2));
+			console.log("Data sent:"+JSON.stringify(school_dropouts[0], null, 2));
+		} else {
+			res.sendStatus(404, "NOT FOUND");
+			console.log("The data requested is empty");
+		}
+	});
+});
+
+//POST school-dropouts/:param
+app.post(BASE_API_URL + "/school-dropouts/:param", (req, res) => {
+	res.sendStatus(405, "METHOD NOT ALLOWED");
+});
+
+//POST school-dropouts/:param1/:param2
+app.post(BASE_API_URL + "/school-dropouts/:param1/:param2", (req, res) => {
+	res.sendStatus(405, "METHOD NOT ALLOWED");
+});
+
+//PUT school-dropouts/:country/:year
+app.put(BASE_API_URL + "/school-dropouts/:country/:year", (req, res) => {
+	console.log("New PUT .../school-dropouts/:country/:year");
+
+	var country = req.params.country;
+	var year = req.params.year;
+	var newSchoolDroupouts = req.body;
+	var query = {"country":country, "year":parseInt(year)};
+
+	if (country != newSchoolDroupouts.country || year != newSchoolDroupouts.year) {
+		res.sendStatus(400, "BAD REQUEST(data does not match)");
+		console.log("Data does not match");
+	} else {
+		db.update(query,newSchoolDroupouts,(err,numReplaced) =>{
+			if(numReplaced == 0){
+				res.sendStatus(400, "BAD REQUEST(there is no such data in the database)");
+				console.log("There is no such data in the database");
+
+			}
+			else{
+				res.sendStatus(200, "OK");
+				console.log("Database updated");
+			}
+		});
+	}
+});
+
+//DELETE school-dropouts/:country/:year
+app.delete(BASE_API_URL + "/school-dropouts/:country/:year", (req, res) => {
+	console.log("New DELETE .../school-dropouts/:country/:year");
+
+	var country = req.params.country;
+	var year = req.params.year;
+	var query = {"country":country, "year":parseInt(year)};
+
+	db.remove(query, {multi:true}, (err, numRemoved) =>{
+		if(numRemoved == 0){
+			res.sendStatus(404, "NOT FOUND");
+			console.log("There is no such data in the database");
+		}
+		else{
+			res.sendStatus(200, "OK");
+			console.log("Object removed");
+		}
+	});
+});
+};
+// -------------------------------------END-SCHOOL-DROPOUTS API-------------------------------------
