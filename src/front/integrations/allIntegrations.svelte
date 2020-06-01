c4d2ff98d17bd02d3955c0174d002a0b0ee4ff7a
Juanjo <juanjoserequenalama@gmail.com>
2020-06-02 01:13:31 +0200

    covid19 is now in this repository

--- src/front/integrations/allIntegrations.svelte
+++ src/front/integrations/allIntegrations.svelte
@@ -18,7 +18,7 @@ function myFunction() {
        <button on:click="{myFunction}"> Integraciones overdose-deaths (Juan Jose Requena Lama) </button>
        
        <div id="divOverdoseDeaths"  style="display:none;" class="list-group"> <br>
-              <a href="#/integrations/ejemplo"  class="list-group-item list-group-item-action list-group-item-success"> Integracion con aplicacion ejemplo</a>
+              <a href="#/integrations/Corona"  class="list-group-item list-group-item-action list-group-item-success"> Integracion con Covid19</a>
               <a href="#/integrations/GeoDb" class="list-group-item list-group-item-action list-group-item-success"> Integracion con GeoDb</a>
               <a href="#/integrations/Quote" class="list-group-item list-group-item-action list-group-item-success"> Integracion con Random Quotes</a>
               <a href="#/integrations/Wikihow" class="list-group-item list-group-item-action list-group-item-success"> Integracion con WikiHow</a>
